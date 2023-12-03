a032f69138be5fec02a8e950fb2b4f938f09506a
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2023-12-03 15:45:26 +0530

    Fix exercise 3.1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,10 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 });
 
+app.get('/api/persons', (request, response) => {
+    response.json(notes);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
