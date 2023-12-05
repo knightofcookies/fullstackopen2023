aba53285ff2fad64bd9a241fb374473ba60ab844
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2023-12-05 14:05:07 +0530

    Configure PORT for production

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -96,7 +96,7 @@ app.get('/api/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date().toString()}</p>`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
