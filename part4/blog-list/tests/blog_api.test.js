f3e73a86b08617f81d8d8f4470d4c3a71c5d8ab7
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2023-12-07 19:56:01 +0530

    Add exercises 4.8 to 4.12

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -10,16 +10,82 @@ const helper = require('./test_helper')
 beforeEach(async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
-})
+}, 10000)
 
 test('blogs are returned as json', async () => {
-  await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
-})
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 10000)
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(helper.initialBlogs.length)
+}, 10000)
+
+test('blogs are returned with id not _id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+}, 10000)
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'Wa',
+    author: 'Authorwa',
+    likes: 999,
+    url: 'foo/bar'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain(
+    'Wa'
+  )
+}, 10000)
+
+test('likes defaults to 0', async () => {
+  const newBlog = {
+    title: 'Wow',
+    author: 'Wowman Wowson'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect(res => res.body.likes === 0)
+}, 10000)
+
+test('returns 400 if title is missing', async () => {
+  const newBlog = {
+    author: 'Guy Ritchie'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+test('returns 400 if author is missing', async () => {
+  const newBlog = {
+    title: 'Star Something'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
 })
 
 afterAll(async () => {
